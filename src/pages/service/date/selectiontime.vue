83f019a111ee27642abb1dc94305ad8a776d7eb3
bbzy <1441106601@qq.com>
2019-11-17 13:46:32 +0800

    *预约时间bug修复*

--- src/pages/service/date/selectiontime.vue
+++ src/pages/service/date/selectiontime.vue
@@ -9,13 +9,13 @@
           header-style="header"
           board-style="board"
           next="false" prev="false" shiow-more-days='true'
-          :days-color="dayStyle" @dayClick="dayClick"  @dateChange="dayChange" weeks-type="cn" @nextMonth="next" @prevMonth="prev"/>
+          :days-color="dayStyle" @dayClick="dayClick" @dateChange="dayChange"  weeks-type="cn" @nextMonth="next" @prevMonth="prev"/>
         </div>
       </div>
       <div class="selectiontime-cont-time">
         <div class="timeBox">
           <ul>
-            <li class="timeBox-list" v-for="(p, i) in periods" :key="i" @click="setPeriod(i)">{{ p }}</li>
+            <li class="timeBox-list" :class="{'on':timeIndex==i}" v-for="(p, i) in periods" :key="i" @click="setPeriod(i)">{{ p }}</li>
           </ul>
         </div>
       </div>
@@ -25,6 +25,9 @@
     <!-- <i-action-sheet :visible="visible1" :actions="actions1" show-cancel @click="handleCancel1" bind:click="handleClickItem1" > -->
     <subscribe-alert-box></subscribe-alert-box>
     <!-- </i-action-sheet> -->
+    <i-modal :visible="visible2" @ok="handleClose2" @cancel="handleClose2">
+        <view>请预约正确的时间</view>
+    </i-modal>
   </div>
 </template>
 
@@ -69,25 +72,39 @@ export default {
 
     ],
     dd: false,
-    yearDate: new Date().getFullYear(),
-    monthDate: (new Date().getMonth()) + 1,
-    dayTime: new Date().getDate()
+    yearDate: null,
+    monthDate: null,
+    dayTime: null,
+    visible2: false,
+    clickDayObj: null,
+    selectDay: null,
+    selectMonth: null,
+    selectYear: null,
+    timeIndex: undefined
   },
   mounted: function () {
+    console.log('我进来了')
     this.getShop()
-
+    this.timeIndex = -1
     let today = new Date().getDate()
     console.log(this.yearDate)
     console.log(this.monthDate)
     console.log(this.dayTime)
     this.dayStyle = []
-    this.dayStyle.push({month: 'current', day: today, color: '#fff', background: '#9E312E', borderRadius: '50%'})
+    this.clickDayObj = null
+    this.setReadOnlyStyle()
+    this.clickDayObj = {month: 'current', day: today, color: '#fff', background: '#9E312E', borderRadius: '50%'}
+    this.selectDay = this.dayTime = new Date().getDate()
+    this.selectMonth = this.monthDate = (new Date().getMonth()) + 1
+    this.selectYear = this.yearDate = new Date().getFullYear()
+    this.dayStyle.push(this.clickDayObj)
   },
   components: {
     subscribeAlertBox
   },
   methods: {
     setPeriod: function (i) {
+      this.timeIndex = i
       this.period = this.periods[i]
     },
     getShop: async function () {
@@ -105,15 +122,39 @@ export default {
       // if (dataB.diff(dataA) > 0) {
       //   this.dayStyle.push({month: 'current', day: '', color: '', background: '', borderRadius: ''})
       // }
-      console.log(1)
-      let clickDay = event.mp.detail.day
+      console.log('d3211231231231233')
+      let clickDay = this.selectDay = event.mp.detail.day
+      let clickMonth = this.selectMonth = event.mp.detail.month
+      let clickYear = this.selectYear = event.mp.detail.year
+      let tmpDate = new Date()
+      tmpDate.setFullYear(clickYear)
+      tmpDate.setMonth(clickMonth - 1)
+      tmpDate.setDate(clickDay)
+      tmpDate.setHours(0)
+      let compareDate = new Date()
+      compareDate.setHours(0)
+      if (tmpDate.getTime() < compareDate.getTime() - 2000) {
+        this.visible2 = true
+        return
+      }
       this.dayTime = event.mp.detail.day
       console.log(clickDay)
-      this.dayStyle.shift()
+      if (!!this.clickDayObj && this.dayStyle.length > 0) {
+        for (var index in this.dayStyle) {
+          if (this.clickDayObj === this.dayStyle[index]) {
+            this.dayStyle.splice(index, 1)
+          }
+        }
+      }
       console.log(this.dayStyle)
-      this.dayStyle.push({month: 'current', day: clickDay, color: 'white', background: '#9E312E', borderRadius: '50%'})
+      this.clickDayObj = {month: 'current', day: clickDay, color: 'white', background: '#9E312E', borderRadius: '50%'}
+      this.dayStyle.push(this.clickDayObj)
       let datStyleForApp = this.dayStyle
       this.$mp.page.setData({datStyleForApp})
+      this.dayTime = this.selectDay
+      console.log(event.mp.detail)
+      this.monthDate = event.mp.detail.month
+      this.yearDate = event.mp.detail.year
       this.$forceUpdate()
     },
     handleOpen1 () {
@@ -144,27 +185,82 @@ export default {
       // this.dd = true
     },
     dayChange (e) {
+      console.log('eweggrg')
       console.log(e)
-      this.commonDate(e)
-      console.log('qqqqqq')
+
+      this.setReadOnlyStyle(e)
+      // this.commonDate(e)
+      console.log(this.dayStyle)
+      // console.log(this.monthDate)
+      // if (this.dayStyle.length !== 0) {
+      //   this.dayStyle[0].background = ''
+      //   this.dayStyle[0].color = ''
+      // }
+      //
+    },
+    setReadOnlyStyle (e) {
+      this.dayStyle = []
+      if (this.clickDayObj) {
+        if (!e) { this.dayStyle.push(this.clickDayObj) } else if (this.selectMonth === e.mp.detail.currentMonth && this.selectYear === e.mp.detail.currentYear) {
+          this.dayStyle.push(this.clickDayObj)
+        }
+      }
+
+      if (!e) {
+        // 首次加载置灰
+        for (let dayTime = 1; dayTime < this.day; dayTime++) {
+          this.dayStyle.push({ month: 'current', day: dayTime, color: '#B7B6B6', background: '' })
+        }
+      } else {
+        console.log(e)
+        e = e.mp.detail
+        console.log(e.currentYear, e.currentMonth)
+        let CompareMonth = new Date(e.currentYear + '-' + e.currentMonth + '-1')
+        let thisMonth = new Date(this.year + '-' + this.month + '-1')
+        if (CompareMonth.getTime() < thisMonth.getTime()) {
+          let tmpDate = new Date()
+          if (e.currentMonth === 12) {
+            tmpDate.setFullYear(e.currentYear + 1)
+            tmpDate.setMonth(1)
+            tmpDate.setDate(1)
+            tmpDate.setHours(0)
+          } else {
+            tmpDate.setFullYear(e.currentYear)
+            tmpDate.setMonth(e.currentMonth)
+            tmpDate.setDate(1)
+            tmpDate.setHours(0)
+          }
+          console.log(new Date(tmpDate.getTime() - 60 * 60 * 1000))
+          let dayLimit = new Date(tmpDate.getTime() - 60 * 60 * 1000).getDate()
+          console.log(e.currentMonth)
+          for (let dayTime = 1; dayTime <= dayLimit; dayTime++) {
+            this.dayStyle.push({ month: 'current', day: dayTime, color: '#B7B6B6', background: '' })
+          }
+          this.$forceUpdate()
+        } else if (CompareMonth.getTime() === thisMonth.getTime()) {
+          for (let dayTime = 1; dayTime < this.day; dayTime++) {
+            this.dayStyle.push({ month: 'current', day: dayTime, color: '#B7B6B6', background: '' })
+          }
+        }
+      }
     },
     next (e) {
-      console.log(e)
-      this.commonDate(e)
-      this.$forceUpdate()
+      this.setReadOnlyStyle(e)
     },
     prev (e) {
-      console.log(e)
-      this.commonDate(e)
+      this.setReadOnlyStyle(e)
     },
     commonDate (e) {
-      this.yearDate = e.target.currentYear
-      this.monthDate = e.target.currentMonth
+      console.log(this.selectDay)
+
       this.dayStyle.shift()
       this.dayStyle.push({month: 'current', day: '', color: '', background: '', borderRadius: ''})
       let datStyleForApp = this.dayStyle
       this.$mp.page.setData({datStyleForApp})
       this.$forceUpdate()
+    },
+    handleClose2 () {
+      this.visible2 = false
     }
   }
 }
